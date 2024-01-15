019434f6f45b74246e52e561a4868401eeeaabc5
Claudio Barsante <clbmribas@gmail.com>
2024-01-15 18:15:22 -0300

    fix: add signal to fetchEvents function

--- src/components/Events/FindEventSection.jsx
+++ src/components/Events/FindEventSection.jsx
@@ -1,12 +1,49 @@
-import { useRef } from 'react';
-
+import { useRef ,useState} from 'react';
+import {useQuery} from '@tanstack/react-query';
+import { fetchEvents } from '../../util/http';
+import LoadingIndicator from '../UI/LoadingIndicator';
+import ErrorBlock from '../UI/ErrorBlock';
+import EventItem from './EventItem';
 export default function FindEventSection() {
   const searchElement = useRef();
+  const [searchTerm,setSearchTerm] = useState('');
+  
+  /*
+  ? signal --
+  TanStack Query provides each query function with an AbortSignal instance. 
+  When a query becomes out-of-date or inactive, this signal will become aborted. 
+  This means that all queries are cancellable, and you can respond to the cancellation
+   inside your query function if desired. The best part about this is that it allows you to 
+   continue to use normal async/await syntax while getting all the benefits of automatic 
+   cancellation.
+  */
+  const {data, isPending,isError,error} = useQuery({
+    queryKey:['elements',{search:searchTerm}],// add one more key to not mix with the results of NewEventsSection
+    queryFn:({signal})=> fetchEvents({signal, search:searchTerm})
+  });
 
   function handleSubmit(event) {
     event.preventDefault();
+    setSearchTerm(searchElement.current.value);
   }
 
+  let content =  <p>Please enter a search term and to find events.</p>;
+
+  if(isPending) content = <LoadingIndicator/>;
+
+  if(isError) content = <ErrorBlock title="An error occurred" message={error.message}/>;
+ 
+  if(data){
+    content = (
+      <ul className="events-list">
+        {data.map((event) => (
+          <li key={event.id}>
+            <EventItem event={event} />
+          </li>
+        ))}
+      </ul>
+    );
+  }
   return (
     <section className="content-section" id="all-events-section">
       <header>
@@ -20,7 +57,7 @@ export default function FindEventSection() {
           <button>Search</button>
         </form>
       </header>
-      <p>Please enter a search term and to find events.</p>
+     {content}
     </section>
   );
 }
