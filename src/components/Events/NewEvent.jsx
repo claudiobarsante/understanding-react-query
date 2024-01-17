2e1e6f2aa20576b7b9d3ee4d65d7c4d0da2def2f
Claudio Barsante <clbmribas@gmail.com>
2024-01-17 16:25:52 -0300

    feat: add invalidateQueries to refetch all querys with queryKey:['events]

--- src/components/Events/NewEvent.jsx
+++ src/components/Events/NewEvent.jsx
@@ -4,12 +4,15 @@ import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
 import { createNewEvent } from '../../util/http.js';
 import ErrorBlock from './../UI/ErrorBlock';
+import { queryClient } from '../../util/http.js';
+
 export default function NewEvent() {
   const navigate = useNavigate();
 
   const {mutate, isPending,isError,error} = useMutation({
     mutationFn: createNewEvent,
     onSuccess: () => {
+      queryClient.invalidateQueries({queryKey:['events']}); // -- tells reacquery that all querys that includes queryKey:['events'] are stale, so they need to be refetched. The events will be refetched in the section 'Recently added  events' for example
       navigate('/events');
     }
   })
